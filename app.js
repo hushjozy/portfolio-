d6957047af7723deaf00b9ac7396bf4c8989b924
JosephOsho <oshojoseph2@gmail.com>
2021-12-08 18:24:11 +0100

    1st portfolio compile

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const request=require("request");
+const nodemailer=require("nodemailer");
 
 const app=express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
-res.sendFile(__dirname + "/signup.html")
+res.sendFile(__dirname + "/index.html")
 })
 app.post("/",function(request,response){
-var firstName=request.body.fName;
-var lastName=request.body.lName;
-var eMail=request.body.email;
-var passWord=request.body.passW;
+var firstName= request.body.name  ;
+var subject="Subject " + request.body.subject;
+var eMail="Email " + request.body.email;
+var message="Message " + request.body.message;
 
-console.log(firstName, lastName, eMail,passWord);
+console.log(firstName, subject, eMail,message);
+response.send("<h1>Your Message has been Sent </h1>" + firstName+ "<h2> We will get Back to you soon. </h2>" );
 })
 app.listen(process.env.PORT||3000,function(){
 console.log("server up & running")
